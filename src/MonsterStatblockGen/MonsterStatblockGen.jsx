4925a6f1055a858d0531ed9e089d5e9c44eb1443
Daniel Shinkarow <41836809+DanShink@users.noreply.github.com>
2025-03-05 21:23:23 -0500

    formatting

--- src/MonsterStatblockGen/MonsterStatblockGen.jsx
+++ src/MonsterStatblockGen/MonsterStatblockGen.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import Typography from '@mui/material/Typography';
-import ProTip from '../ProTip';
+import React from "react";
+import Typography from "@mui/material/Typography";
+import ProTip from "../ProTip";
 
 export default function MonsterStatblockGen() {
-  return (
-    <React.Fragment>
-      <Typography variant="h4" component="h1" sx={{ mb: 2 }} style={{ display: 'flex', textAlign: 'center'}}>
-        Monster Statblock Generator for DC20
-      </Typography>
-      <div>
-        CONTENT
-      </div>
-      <ProTip />
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Typography
+				variant="h4"
+				component="h1"
+				sx={{ mb: 2 }}
+				style={{ display: "flex", textAlign: "center" }}
+			>
+				Monster Statblock Generator for DC20
+			</Typography>
+			<div>CONTENT</div>
+			<ProTip />
+		</React.Fragment>
+	);
 }
