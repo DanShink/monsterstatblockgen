9615b095eab2bc735b8a94a06d7d1bda03b021cd
Daniel Shinkarow <41836809+DanShink@users.noreply.github.com>
2025-03-05 21:16:26 -0500

    feat: setup directories

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import ProTip from './ProTip';
+import MonsterStatblockGen from './MonsterStatblockGen/MonsterStatblockGen';
 
 export default function App() {
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }} style={{ display: 'flex', textAlign: 'center'}}>
-          Monster Statblock Generator for DC20
-        </Typography>
-        <ProTip />
+        <MonsterStatblockGen />
       </Box>
     </Container>
   );
