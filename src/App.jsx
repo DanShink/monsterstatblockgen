e1e650758f53a69e046ebe103bbaf14f507362f6
Daniel Shinkarow <41836809+DanShink@users.noreply.github.com>
2025-03-06 17:27:08 -0500

    feat: increase size of container so tabs won't get cut off

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box';
-import MonsterStatblockGen from './MonsterStatblockGen/MonsterStatblockGen';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import MonsterStatblockGen from "./MonsterStatblockGen/MonsterStatblockGen";
 
 export default function App() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
-        <MonsterStatblockGen />
-      </Box>
-    </Container>
-  );
+	return (
+		<Container maxWidth="md">
+			<Box
+				sx={{ my: 4 }}
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					alignItems: "center",
+					flexDirection: "column",
+				}}
+			>
+				<MonsterStatblockGen />
+			</Box>
+		</Container>
+	);
 }
