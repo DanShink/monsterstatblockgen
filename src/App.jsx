61fa77a5ac0aca627e20fbf0627267d28617e270
Daniel Shinkarow <41836809+DanShink@users.noreply.github.com>
2025-03-05 21:02:48 -0500

    feat: boilerplate text changed for project title

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ProTip from './ProTip';
-import Copyright from './Copyright';
 
 export default function App() {
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI Vite.js example
+      <Box sx={{ my: 4 }} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'}}>
+        <Typography variant="h4" component="h1" sx={{ mb: 2 }} style={{ display: 'flex', textAlign: 'center'}}>
+          Monster Statblock Generator for DC20
         </Typography>
         <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
