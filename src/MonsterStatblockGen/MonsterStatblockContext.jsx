94069066827429e883df4400c7c8b53508d9d035
Colek45 <32225029+Colek45@users.noreply.github.com>
2025-03-07 19:11:59 -0700

    Added stats page functionality

--- src/MonsterStatblockGen/MonsterStatblockContext.jsx
+++ src/MonsterStatblockGen/MonsterStatblockContext.jsx
@@ -10,14 +10,14 @@ function useState() {
   const [category, setCategory] = React.useState("Minion");
   const [apex, setApex] = React.useState(false);
   const [hp, setHp] = React.useState("");
-  const [mig, setMig] = React.useState("");
-  const [agl, setAgl] = React.useState("");
-  const [cha, setCha] = React.useState("");
-  const [int, setInt] = React.useState("");
-  const [migprof, setMigprof] = React.useState("");
-  const [aglprof, setAglprof] = React.useState("");
-  const [chaprof, setChaprof] = React.useState("");
-  const [intprof, setIntprof] = React.useState("");
+  const [mig, setMig] = React.useState(0);
+  const [agl, setAgl] = React.useState(0);
+  const [cha, setCha] = React.useState(0);
+  const [int, setInt] = React.useState(0);
+  const [migprof, setMigprof] = React.useState(false);
+  const [aglprof, setAglprof] = React.useState(false);
+  const [chaprof, setChaprof] = React.useState(false);
+  const [intprof, setIntprof] = React.useState(false);
 
   return {
     name,
@@ -36,6 +36,20 @@ function useState() {
     setHp,
     mig,
     setMig,
+    agl,
+    setAgl,
+    cha,
+    setCha,
+    int,
+    setInt,
+    migprof,
+    setMigprof,
+    aglprof,
+    setAglprof,
+    chaprof,
+    setChaprof,
+    intprof,
+    setIntprof,
   };
 }
 
