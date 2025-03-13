a07b360a3b67efb7a79f17eda3268ed29e563ed3
Daniel Shinkarow <41836809+DanShink@users.noreply.github.com>
2025-03-12 20:09:01 -0400

    add resistances

--- src/MonsterStatblockGen/MonsterStatblockContext.jsx
+++ src/MonsterStatblockGen/MonsterStatblockContext.jsx
@@ -3,66 +3,72 @@ import React from "react";
 const MonsterStatblockContext = React.createContext();
 
 function useState() {
-  const [name, setName] = React.useState("");
-  const [size, setSize] = React.useState("Medium");
-  const [type, setType] = React.useState("Humanoid");
-  const [level, setLevel] = React.useState(0);
-  const [category, setCategory] = React.useState("Minion");
-  const [apex, setApex] = React.useState(false);
-  const [hp, setHp] = React.useState("");
-  const [mig, setMig] = React.useState(0);
-  const [agl, setAgl] = React.useState(0);
-  const [cha, setCha] = React.useState(0);
-  const [int, setInt] = React.useState(0);
-  const [migprof, setMigprof] = React.useState(false);
-  const [aglprof, setAglprof] = React.useState(false);
-  const [chaprof, setChaprof] = React.useState(false);
-  const [intprof, setIntprof] = React.useState(false);
+	const [name, setName] = React.useState("");
+	const [size, setSize] = React.useState("Medium");
+	const [type, setType] = React.useState("Humanoid");
+	const [level, setLevel] = React.useState(0);
+	const [category, setCategory] = React.useState("Minion");
+	const [apex, setApex] = React.useState(false);
+	const [hp, setHp] = React.useState("");
+	const [mig, setMig] = React.useState(0);
+	const [agl, setAgl] = React.useState(0);
+	const [cha, setCha] = React.useState(0);
+	const [int, setInt] = React.useState(0);
+	const [migprof, setMigprof] = React.useState(false);
+	const [aglprof, setAglprof] = React.useState(false);
+	const [chaprof, setChaprof] = React.useState(false);
+	const [intprof, setIntprof] = React.useState(false);
+	const [resistances, setResistances] = React.useState({
+		bludgeoning: "0",
+		piercing: "0",
+	});
 
-  return {
-    name,
-    setName,
-    size,
-    setSize,
-    type,
-    setType,
-    level,
-    setLevel,
-    category,
-    setCategory,
-    apex,
-    setApex,
-    hp,
-    setHp,
-    mig,
-    setMig,
-    agl,
-    setAgl,
-    cha,
-    setCha,
-    int,
-    setInt,
-    migprof,
-    setMigprof,
-    aglprof,
-    setAglprof,
-    chaprof,
-    setChaprof,
-    intprof,
-    setIntprof,
-  };
+	return {
+		name,
+		setName,
+		size,
+		setSize,
+		type,
+		setType,
+		level,
+		setLevel,
+		category,
+		setCategory,
+		apex,
+		setApex,
+		hp,
+		setHp,
+		mig,
+		setMig,
+		agl,
+		setAgl,
+		cha,
+		setCha,
+		int,
+		setInt,
+		migprof,
+		setMigprof,
+		aglprof,
+		setAglprof,
+		chaprof,
+		setChaprof,
+		intprof,
+		setIntprof,
+		resistances,
+		setResistances,
+	};
 }
 
 export function useMonsterStatblockContext() {
-  const state = React.useContext(MonsterStatblockContext);
-  return state;
+	const state = React.useContext(MonsterStatblockContext);
+	return state;
 }
 
 export function MonsterStatBlockContextProvider({ children }) {
-  const state = useState();
-  return (
-    <MonsterStatblockContext.Provider value={state}>
-      {children}
-    </MonsterStatblockContext.Provider>
-  );
+	const state = useState();
+	return (
+		<MonsterStatblockContext.Provider value={state}>
+			{children}
+		</MonsterStatblockContext.Provider>
+	);
 }
