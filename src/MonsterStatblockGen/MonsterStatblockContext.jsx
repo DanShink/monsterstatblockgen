9ec3af6c3cc33c083105ff2c51fc59730f72566b
Colek45 <32225029+Colek45@users.noreply.github.com>
2025-03-14 15:47:06 -0700

    Context

--- src/MonsterStatblockGen/MonsterStatblockContext.jsx
+++ src/MonsterStatblockGen/MonsterStatblockContext.jsx
@@ -75,6 +75,13 @@ function useState() {
 	distance:0,
   }*/
   //features page
+  const [features, setFeatures] = React.useState([]);
+  /* Expected objects to be pushed: {
+ 	feature:"",
+	description:"", 
+  }
+
+  */
 
   return {
     name,
